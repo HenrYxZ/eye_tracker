e4554952f491e0ab5c2c791a486300a96ef1fad0
HenrY <hjhenriq@gmail.com>
2014-06-30 12:47:25 -0400

    untested all the logic to draw a circle in the eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from segmentation import segmentate
 
 '''
 This project is an eye tracker that gets a video of an eye watching another 
@@ -25,13 +26,19 @@ def get_videos(source_1, source_2):
 
 	return first_cap, second_cap
 
-def report(log, frame_number, center, radious):
+def report(log, frame_number, center, radius):
 	header = "New frame number :" + str(frame_number) + "\n"
 	log.write(header)
 	line = "Center: ({0},{1})\n"
 	log.write(line.format(str(center.x), str(center.y)))
-	line = "Radious: {0}\n\n"
-	log.write(line.format(str(radious))) 
+	line = "Radius: {0}\n\n"
+	log.write(line.format(str(radius))) 
+
+def get_circle(cnt):
+	(x,y),radius = cv2.minEnclosingCircle(cnt)
+	center = (int(x),int(y))
+	radius = int(radius)
+	return center, radius
 
 def process_by_frame:
 	log = open("log.txt", "w")
@@ -41,8 +48,13 @@ def process_by_frame:
 		ret_looked, img_looked = looked_capture.read()
 		if not ret_eye or not ret_looked:
 			break
+		new_eye_img = cv2.cv.cvCloneImage(img_eye)
 		pupil = segmentate(img_eye)
-
+		# TODO process pupil
+		center, radius = get_circle(pupil)
+		border_color = (0, 255, 0)
+		cv2.circle(new_eye_img, center, radius, border_color, 2)
+		cv2.imshow("Eye segmentation", new_eye_img)
 
 	log.close()
 
@@ -53,6 +65,7 @@ def main:
 	print ('Write the path for the looked video')
 	looked_video_src = raw_input()
 
+	cv2.namedWindow("Eye segmentation")
 	eye_capture, looked_capture = get_videos(eye_video_src, looked_video_src)
 	process_by_frame(eye_capture, looked_capture)
 
